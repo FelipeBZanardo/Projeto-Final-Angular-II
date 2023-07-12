448228ad3724f7861036482c534c98573d5fc5f5
Felipe Zanardo <felipebueno6@hotmail.com>
2023-07-12 09:17:35 -0300

    feat: implementação do módulo de autenticação

---
+++ src/app/modules/auth/auth.module.ts
@@ -0,0 +1,25 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { AuthRoutingModule } from './auth-routing.module';
+import { LoginComponent } from './components/login/login.component';
+
+
+@NgModule({
+  declarations: [LoginComponent],
+  imports: [
+    AuthRoutingModule,
+    CommonModule,
+    FormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatInputModule,
+  ]
+})
+export class AuthModule { }
---
+++ src/app/modules/auth/components/login/login.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/models/user.model';
+import { UsersService } from 'src/app/modules/users/services/users.service';
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  
+  public username?: string;
+  public password?: string;
+  public users!: User[];
+
+  constructor(private router: Router, private authService: AuthService, private usersService: UsersService) {}
+
+  ngOnInit(): void {
+    this.users = this.usersService.findAll();
+  }
+
+  public login(): void {
+    const result = this.authService.login({
+      username: this.username,
+      password: this.password,
+    });
+
+    console.log(typeof result);
+
+    if (typeof result === 'string') {
+      console.log(result);
+      return;
+    }
+
+    this.router.navigate(['/users']);
+  }
+}
