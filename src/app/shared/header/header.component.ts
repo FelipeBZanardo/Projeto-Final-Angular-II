cc9a9708e3f84f55a83ebe63a2c5aea3f6fd87d8
Felipe Zanardo <felipebueno6@hotmail.com>
2023-07-12 09:26:34 -0300

    feat: implementação do menu de navegação comum em toda a aplicação

---
+++ src/app/shared/header/header.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/modules/auth/services/auth.service';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+
+  constructor (private authService: AuthService, private router: Router) {}
+
+  public logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/auth/login']);
+  }
+}
