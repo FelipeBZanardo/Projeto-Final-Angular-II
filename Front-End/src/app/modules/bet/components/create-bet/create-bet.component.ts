0fe673d8433154bac095a2236d649e352b3d3223
Ana Beatriz Trindade <anabeatrizdmt@hotmail.com>
2023-07-14 00:08:41 -0300

    feat: adding forms masks and validations

--- Front-End/src/app/modules/bet/components/create-bet/create-bet.component.ts
+++ Front-End/src/app/modules/bet/components/create-bet/create-bet.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormArray,
+  FormControl,
+  FormGroup,
+  ValidationErrors,
+  Validators,
+} from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Bet } from 'src/app/models/bet.model';
 import { BetService } from '../../services/bet.service';
@@ -7,7 +14,7 @@ import { BetService } from '../../services/bet.service';
 @Component({
   selector: 'app-create-bet',
   templateUrl: './create-bet.component.html',
-  styleUrls: ['./create-bet.component.css']
+  styleUrls: ['./create-bet.component.css'],
 })
 export class CreateBetComponent implements OnInit {
   public bet?: Bet;
@@ -16,13 +23,17 @@ export class CreateBetComponent implements OnInit {
 
   public betForm!: FormGroup;
 
-  constructor(private router: Router, private route: ActivatedRoute, private betService: BetService) {}
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private betService: BetService
+  ) {}
 
   ngOnInit(): void {
     this.buildForm();
 
     this.id = this.route.snapshot.params['id'];
-    
+
     if (this.id) {
       this.bet = this.betService.findById(this.id);
       this.title = 'Editar Aposta';
@@ -31,32 +42,84 @@ export class CreateBetComponent implements OnInit {
   }
 
   public buildForm(): void {
+    const uniqueNumbersValidator = (
+      control: AbstractControl
+    ): ValidationErrors | null => {
+      const dozens = control.value;
+      const selectedNumbers = dozens.map((dozen: any) => dozen.number);
+
+      const uniqueNumbers = [...new Set(selectedNumbers)];
+
+      if (uniqueNumbers.length === selectedNumbers.length) {
+        return null;
+      } else {
+        return { duplicateNumbers: true };
+      }
+    };
+
     this.betForm = new FormGroup({
       id: new FormControl(),
       raffleNumber: new FormControl(null, [Validators.required]),
-      dozens: new FormArray([
-        new FormGroup({number: new FormControl(null, [Validators.required, Validators.min(1), Validators.max(80)])}),
-        new FormGroup({number: new FormControl(null, [Validators.required, Validators.min(1), Validators.max(80)])}),
-        new FormGroup({number: new FormControl(null, [Validators.required, Validators.min(1), Validators.max(80)])}),
-        new FormGroup({number: new FormControl(null, [Validators.required, Validators.min(1), Validators.max(80)])}),
-        new FormGroup({number: new FormControl(null, [Validators.required, Validators.min(1), Validators.max(80)])})
+      dozens: new FormArray(
+        [
+          new FormGroup({
+            number: new FormControl(null, [
+              Validators.required,
+              Validators.min(1),
+              Validators.max(80),
+            ]),
+          }),
+          new FormGroup({
+            number: new FormControl(null, [
+              Validators.required,
+              Validators.min(1),
+              Validators.max(80),
+            ]),
+          }),
+          new FormGroup({
+            number: new FormControl(null, [
+              Validators.required,
+              Validators.min(1),
+              Validators.max(80),
+            ]),
+          }),
+          new FormGroup({
+            number: new FormControl(null, [
+              Validators.required,
+              Validators.min(1),
+              Validators.max(80),
+            ]),
+          }),
+          new FormGroup({
+            number: new FormControl(null, [
+              Validators.required,
+              Validators.min(1),
+              Validators.max(80),
+            ]),
+          }),
+        ],
+        { validators: uniqueNumbersValidator }
+      ),
+      betDate: new FormControl(null, [
+        Validators.required,
+        Validators.pattern(/^\d{2}\d{2}\d{4}$/),
       ]),
-      betDate: new FormControl(null, [Validators.required]),
     });
   }
 
   get dozens() {
-    return this.betForm.controls["dozens"] as FormArray;
+    return this.betForm.controls['dozens'] as FormArray;
   }
 
   private updateForm(): void {
     const dozensForm = this.betForm.get('dozens') as FormArray;
     dozensForm.setValue([
-      {number: this.bet?.dozens[0]},
-      {number: this.bet?.dozens[1]},
-      {number: this.bet?.dozens[2]},
-      {number: this.bet?.dozens[3]},
-      {number: this.bet?.dozens[4]}]);
+      { number: this.bet?.dozens[0] },
+      { number: this.bet?.dozens[1] },
+      { number: this.bet?.dozens[2] },
+      { number: this.bet?.dozens[3] },
+      { number: this.bet?.dozens[4] },
+    ]);
     this.betForm.patchValue(this.bet as Bet);
   }
 
@@ -72,5 +135,4 @@ export class CreateBetComponent implements OnInit {
   public onCancel(): void {
     this.router.navigate(['/bet']);
   }
-
 }
