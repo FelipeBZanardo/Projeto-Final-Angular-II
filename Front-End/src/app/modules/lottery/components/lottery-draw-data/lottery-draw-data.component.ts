1ff9bb472eda0feef9919b915ea9177d37a28c46
Adriano Dias <adriano.rj.br@gmail.com>
2023-07-17 19:26:45 -0300

    teste de uso de model no template

--- Front-End/src/app/modules/lottery/components/lottery-draw-data/lottery-draw-data.component.ts
+++ Front-End/src/app/modules/lottery/components/lottery-draw-data/lottery-draw-data.component.ts
@@ -1,39 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { LotteryService } from '../../services/lottery.service';
 import { SnackbarService } from 'src/app/shared/services/snackbar.service';
+import { LotteryDraw } from '../../models/lottery-draw.model';
 
 @Component({
   selector: 'app-lottery-draw-data',
   templateUrl: './lottery-draw-data.component.html',
-  styleUrls: ['./lottery-draw-data.component.css']
+  styleUrls: ['./lottery-draw-data.component.css'],
 })
 export class LotteryDrawDataComponent implements OnInit {
-[x: string]: any;
-// public drawData?: LotteryDraw;
-public drawData?: string;
-private localStorageToken = localStorage.getItem('TOKEN');
+  [x: string]: any;
+  public drawData?: LotteryDraw;
+  // public drawData?: string;
+  private localStorageToken = localStorage.getItem('TOKEN');
   constructor(
     private lotteryService: LotteryService,
     private snackbarService: SnackbarService
-    ) {}
-  
+  ) {}
+
   ngOnInit(): void {
-    this.getDrawData();      
+    this.getDrawData();
   }
 
   public getDrawData(): void {
-    this.lotteryService
-      .getLotteryLastDrawData()
-      .subscribe({
-        next: (response) => {
-          this.drawData =JSON.stringify(response);
-        },
-        error: (err) => {
-          this.snackbarService.openSnackBar(err.message);
-        },
-      });
+    this.lotteryService.getLotteryLastDrawData().subscribe({
+      next: (response) => {
+        // this.drawData =JSON.stringify(response);
+        this.drawData = response;
+      },
+      error: (err) => {
+        this.snackbarService.openSnackBar(err.message);
+      },
+    });
   }
-
-
 }
-
