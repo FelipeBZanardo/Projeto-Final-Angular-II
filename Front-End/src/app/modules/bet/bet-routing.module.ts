ed5f59ae09469e12f6bef835b6eaa813d3f59ecb
Felipe Zanardo <felipebueno6@hotmail.com>
2023-07-21 19:14:58 -0300

    feat: acréscimo das regras de navegação devido aos guards

--- Front-End/src/app/modules/bet/bet-routing.module.ts
+++ Front-End/src/app/modules/bet/bet-routing.module.ts
@@ -2,19 +2,32 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreateBetComponent } from './components/create-bet/create-bet.component';
 import { ListBetComponent } from './components/list-bet/list-bet.component';
+import { authGuard } from 'src/app/core/guards/functional-auth.guard';
 
 const routes: Routes = [
   {
     path: '',
     component: ListBetComponent,
+    canActivate: [authGuard],
+    data: {
+      roles: ['ADMIN', 'USER'],
+    },
   },
   {
     path: 'create',
     component: CreateBetComponent,
+    canActivate: [authGuard],
+    data: {
+      roles: ['ADMIN', 'USER'],
+    },
   },
   {
     path: 'edit/:id',
     component: CreateBetComponent,
+    canActivate: [authGuard],
+    data: {
+      roles: ['ADMIN', 'USER'],
+    },
   },
 ];
 
