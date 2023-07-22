2ebf81438fa659d56dfd8ff1b3c61a4a8cf29476
Felipe Zanardo <felipebueno6@hotmail.com>
2023-07-22 15:40:44 -0300

    style: menu responsivo

--- Front-End/src/app/shared/shared.module.ts
+++ Front-End/src/app/shared/shared.module.ts
@@ -1,12 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from "@angular/flex-layout";
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { AppRoutingModule } from '../app-routing.module';
 import { HeaderComponent } from './header/header.component';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
   declarations: [HeaderComponent, NotFoundComponent],
@@ -17,6 +20,9 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
     MatButtonModule,
     MatIconModule,
     MatSnackBarModule,
+    FlexLayoutModule,
+    MatSidenavModule,
+    MatListModule
   ],
   exports: [HeaderComponent, NotFoundComponent],
 })
